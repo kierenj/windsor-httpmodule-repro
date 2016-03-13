6f8ab9101b7c634b6019dcd866c4f783a9da5bdd
kierenj <kierenj@gmail.com>
2016-03-13 18:55:32 +0000

    Minimal repro project

---
+++ Global.asax.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+
+namespace WebApplication1
+{
+    public interface IFoobar
+    {
+    }
+
+    public class Foobar : IFoobar
+    {
+    }
+
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            var container = new WindsorContainer();
+            container.Register(Component.For<IFoobar>().ImplementedBy<Foobar>().LifestylePerWebRequest());
+
+            // blows up here
+            var foobar = container.Resolve<IFoobar>();
+        }
+    }
+}
